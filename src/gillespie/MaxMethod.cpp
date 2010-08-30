04753e2a84060af761f4db906ba349a8f9ca228b
Gerd Zschaler <gerd@gzschaler.de>
2010-08-30 16:19:42 +0200

    Fixed Gillespie MaxMethod, uses STL algorithms now

--- src/gillespie/MaxMethod.cpp
+++ src/gillespie/MaxMethod.cpp
@@ -7,6 +7,13 @@
 #include "MaxMethod.h"
 #include <myrng1.2/myrngWELL.h>
 #include <vector>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <boost/bind.hpp>
+
+using namespace std;
+using namespace boost;
 
 namespace sim
 {
@@ -24,30 +31,34 @@ void MaxMethod::registerProcess(Process* p)
 
 double MaxMethod::step()
 {
-	typedef std::vector<double> RateVector;
-	RateVector max_rates, max_rates_times_sizes;
-	max_rates.reserve(processes_.size());
-	max_rates_times_sizes.reserve(processes_.size());
-	double total_max_rate_times_size = 0.0;
-	for (ProcessesVector::iterator it = processes_.begin(); it
-			!= processes_.end(); ++it)
-	{
-		double max_rate = it->maxRate();
-		total_max_rate_times_size += max_rate;
-		max_rates.push_back(max_rate);
-		max_rates_times_sizes.push_back(max_rate * it->populationSize());
-	}
+	typedef vector<double> RateVector;
+	RateVector max_rates(processes_.size(), 0.0), max_rates_times_sizes(
+			processes_.size(), 0.0);
 
-	double tau = 0, inv_total_max_rate_times_size = 1.0 / total_max_rate_times_size;
+	// fill max_rates vector
+	transform(processes_.begin(), processes_.end(), max_rates.begin(),
+			bind(&Process::maxRate, _1));
+
+	// fill max_rates_times_sizes vector
+	transform(processes_.begin(), processes_.end(), max_rates.begin(),
+			max_rates_times_sizes.begin(), bind(multiplies<double>(), bind(&Process::populationSize, _1), _2));
+
+	// calculate 1 / sum(max_rate*pop_size)
+	double inv_total_max_rate_times_size = 1.0 / accumulate(
+			max_rates_times_sizes.begin(), max_rates_times_sizes.end(), 0.0);
+
+	// calculate probabilities for each process
+	// FIXME: potential performance issue
+	RateVector process_probs(processes_.size(), 0.0);
+	transform(max_rates_times_sizes.begin(), max_rates_times_sizes.end(),
+			process_probs.begin(), bind(multiplies<double>(), _1, inv_total_max_rate_times_size));
+	double tau = 0;
 	while (true)
 	{
+		// draw length of this time step from exponential distribution
 		tau += rng.Exponential(inv_total_max_rate_times_size);
-		double x = rng.Uniform01() * total_max_rate_times_size;
-		unsigned int k = 0;
-		double s = max_rates_times_sizes[k];
-		while (s < x)
-			s += max_rates_times_sizes[++k];
-		// now, try to execute process
+		// pick process with probability max_rate * pop_size / sum(max_rate * pop_size)
+		int k = rng.Choices(&(process_probs.front()), process_probs.size());
 		Agent* a = processes_[k].randomAgent();
 		double prob = processes_[k].agentRate(a) / max_rates[k];
 		if (rng.Chance(prob))
